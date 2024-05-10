797fba3f4d18e8d1a631ac3709f30496c1dd0f3a
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-09 22:57:31 -0500

    Modificando la clase PRESTAMO DAO

--- src/main/java/interfacesDao/PrestamoDao.java
+++ src/main/java/interfacesDao/PrestamoDao.java
@@ -1,16 +1,18 @@
-
 package interfacesDao;
 
+import Models.Libros;
 import Models.Prestamos;
+import Models.Usuarios;
 import java.util.List;
 
 public interface PrestamoDao {
+
     //metodos 
     public void regristrar(Prestamos prestamo);
 
     public void modificar(Prestamos prestamo);
 
-    //public void eliminar(Prestamos prestamo);
+    public Prestamos ObtenerPrestamos(Usuarios usuario, Libros libro);
 
     public List<Prestamos> listar();
 }
